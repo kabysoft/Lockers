b45d8ae9ce245258c5ae00b3f7626ead0ca0b4f5
kabysoft <kabysoft@yahoo.fr>
2022-08-08 21:54:48 -0600

    4 Commit Add delete file  Methode to project

--- src/Lockers_Pvt_Ltd/FileHandler.java
+++ src/Lockers_Pvt_Ltd/FileHandler.java
@@ -3,7 +3,9 @@ package Lockers_Pvt_Ltd;
 
 
 	import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import java.util.Scanner;
 
 	/**
 	 * File Handler logic implementation goes here Please complete these methods.
@@ -14,13 +16,35 @@ import java.util.List;
 
 		@Override
 		public void addFile(String fileName) {
+			Scanner sc = new Scanner(System.in);
+			String subOption = sc.next();
+
+			try {
 			
+				File myObj = new File("./MainFolder",subOption);
+			      if (myObj.createNewFile()) {
+			        System.out.println("File created: " + myObj.getName());
+			      } else {
+			        System.out.println("File already exists.");
+			      }
+			    } catch (IOException e) {
+			      System.out.println("An error occurred.");
+			      e.printStackTrace();
+			    }
 
 		}
 
 		@Override
 		public void deleteFile(String fileName) {
-
+			Scanner sc = new Scanner(System.in);
+			String subOption = sc.next();
+			File myObj = new File("./MainFolder",subOption); 
+				
+			    if (myObj.delete()) { 
+			      System.out.println("Deleted  file: " + myObj.getName()+ " Successful Operation");
+			    } else {
+			      System.out.println("Failed to delete the file, Respect upper and lower case letters, Or File Not exist");
+			    } 
 		}
 
 		public List<String> listFiles() {
--- src/Lockers_Pvt_Ltd/LockersPvt.java
+++ src/Lockers_Pvt_Ltd/LockersPvt.java
@@ -69,15 +69,20 @@ public class LockersPvt {
 			String subOption = sc.next();
 
 			if ("2.1".equals(subOption)) {
+				fm.addFile(subOption);
+					
 				
 				
-				System.out.println("Add file logic here");
 			} else if ("2.2".equals(subOption)) {
-				System.out.println("Delete file logic here..");
+				fm.deleteFile(subOption);
+				
 			} else if ("2.3".equals(subOption)) {
 				System.out.println("Search file logic here..");
 			} else if ("2.4".equals(subOption)) {
-				System.out.println(" Getting back to main menu");
+				System.out.println("=======================================");
+				System.out.println("||           Main Menu               ||");
+				System.out.println("=======================================");
+				//System.out.println(" Getting back to main menu");
 				break;
 			} else {
 				System.out.println("Invalido Option : Please select again");
@@ -87,7 +92,10 @@ public class LockersPvt {
 
 	private static String detailedMenuOptions() {
 		StringBuilder sb = new StringBuilder();
-		sb.append("2.1. 1Add File \n");
+		System.out.println("========================================================");
+    	System.out.println("||*******      Detailed Options      *******           ||");
+    	System.out.println("=========================================================");
+		sb.append("2.1. Add File \n");
 		sb.append("2.2. Delete File \n");
 		sb.append("2.3. Search File \n");
 		sb.append("2.4. Return Main Menu \n");
@@ -96,6 +104,9 @@ public class LockersPvt {
 
 	private static String mainMenuOptions() {
 		StringBuilder sb = new StringBuilder();
+		System.out.println("=======================================");
+		System.out.println("||           Main Menu               ||");
+		System.out.println("=======================================");
 		sb.append("1. List All Files \n");
 		sb.append("2. Detailed Options \n");
 		sb.append("3. Close Application \n");
