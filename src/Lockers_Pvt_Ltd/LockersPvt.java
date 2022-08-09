00bc673f0ee3ce62f25510e820cabd57a073846b
kabysoft <kabysoft@yahoo.fr>
2022-08-08 18:32:53 -0600

    My First Commit

---
+++ src/Lockers_Pvt_Ltd/LockersPvt.java
@@ -0,0 +1,20 @@
+package Lockers_Pvt_Ltd;
+
+public class LockersPvt {
+
+	public static void main(String[] args) {
+		System.out.println("===============================================================================");
+		System.out.println("/////////////////   Welcome to Company LOCKERS  Pvt. Ltd ! ////////////////////");
+		System.out.println("----------------  Application Name : Lockers Degital  V B1.1 ------------------");
+		System.out.println("===============================================================================");
+		System.out.println("====================================");
+		System.out.println("||Developer Name: SALAH BOUTEMEUR ||");
+		System.out.println("====================================");
+		
+		System.out.println("!");
+		System.out.println("! ");
+		
+		System.out.println("Please Enter one of below options:");
+	}
+
+}
